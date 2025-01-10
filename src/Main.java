340b19de309664b18c710efb145718d4af44bc3a
BT_1N3_13 <safwans2789@nycstudents.net>
2025-01-10 10:45:59 -0500

    init

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("our first shared repo");
-        System.out.println(Math.sin(Math.PI * 2));
+        System.out.println(Math.sin(Math.PI * 4));
     }
 }
